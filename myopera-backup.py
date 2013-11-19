d423d04b2cb2af18c51881a892e62b543b5f0ce1
Frans de Jonge <fransdejonge@gmail.com>
2013-11-19 21:54:36 +0100

    Added some error handling.

--- myopera-backup.py
+++ myopera-backup.py
@@ -35,6 +35,8 @@ counter = 111 # regular old public forum
 #counter = 14782702
 #test for private groups
 #counter = 14951902
+#test for comment that does not exist
+#counter = 14951903
 
 
 # Shortcut for sleep
@@ -49,7 +51,10 @@ for comment_id in range(counter, counter + 2):
 	#comment_id = str(counter)
 	comment_id = str(comment_id)
 	
-	print('for loop at comment '+comment_id)
+	print('Processing comment '+comment_id+'.')
+	
+	# Let's not be too hasty after the last request
+	wait()
 
 	###################
 	# Directory generation here so we can check if file exists; if it does, SKIP one iteration.
@@ -89,11 +94,17 @@ for comment_id in range(counter, counter + 2):
 
 	# Skip this iteration if the comment was already downloaded
 	if os.path.exists(comment_file_name):
+		print('Skipping '+comment_id+'. File exists.')
 		continue
 
 	# Only get headers
 	# No authorization required
 	header_request = requests.head('http://my.opera.com/community/forums/findpost.pl?id='+comment_id)
+	
+	# Skip this iteration if the comment doesn't exist
+	if header_request.ok is False:
+		print('Skipping '+comment_id+'. Does not exist on server.')
+		continue
 
 	# findpost.pl redirects, find out where
 	location = header_request.headers['location']
@@ -111,111 +122,109 @@ for comment_id in range(counter, counter + 2):
 
 	# Construct quote URL
 	quote_request_url = 'http://my.opera.com/community/forums/reply.dml?action=quote&commentid='+comment_id+'&id='+topic_id
-	print(quote_request_url)
+	#print(quote_request_url)
 
 	quote_request = requests.get(quote_request_url, auth=credentials)
 
-	quote_page = quote_request.text
-
 	# True if HTTP status 200; False for e.g. 401 or 404. Maybe check actual error codes and raise an alarm if it's not 401 or 404? Or write some kind of error log under else?
-	if (quote_request.ok):
-		#print(quote_page)
-		
-		metadata = re.search(r'<div id="forumnav"><p class="forumnav"><a href="/[\w]+/forums/">Forums</a>  » <a href="forum\.dml\?id=([0-9]+)">(.+?)</a> » <a href="topic\.dml\?id=[0-9]+">(.+?)</a>', quote_page)
-		
-		#print(metadata.group(0))
-		forum_id = metadata.group(1)
-		forum_name = metadata.group(2)
-		topic_title = metadata.group(3)
-		print(forum_id)
-		print(forum_name)
-		print(topic_title)
-		
-		# re.DOTALL makes dot also match newlines
-		post = re.search(r'<textarea name="comment" id="postcontent" rows="20" cols="60">\[quote=([\w]+)](.+?)\[/quote] </textarea>', quote_page, re.DOTALL)
-		user = post.group(1)
-		post_text = post.group(2)
-		
-		print(user)
-		#print(post_text)
-		
-		# Decode HTML entities
-		# Thanks to http://stackoverflow.com/a/2087433
-		import html.parser
-		h = html.parser.HTMLParser()
-		post_text = h.unescape(post_text)
-		
-		print(post_text)
-		
-		#################
-		#we're still missing the forum category; grab it using the topic id or forum id
-		#forum_id with perscreen=1 is most likely the smallest
-		category_request_url = 'http://my.opera.com/community/forums/forum.dml?id='+forum_id+'&perscreen=1'
-		category_request = requests.get(category_request_url, auth=credentials)
-		
-		metadata = re.search(r'<p class="forumnav"><a href="/[\w]+/forums/">Forums</a>   » <a href="/community/forums/tgr.dml\?id=([0-9]+)" dir="ltr">(.+?)</a>', category_request.text)
-		
-		forum_category_id = metadata.group(1)
-		forum_category = metadata.group(2)
-		
-		#print(forum_category_id)
-		#print(forum_category)
-		
-		
-		# write post file
-		# format something simple and logical, e.g.
-		'''
-		user
-		comment_id
-		timestamp
-		forum_category_id
-		forum_category
-		forum_id
-		forum_name
-		topic_id
-		topic_title
-
-		post_text
-		'''
-		# in this format line 1 is user name, line 2 is comment id, etc. line 10 (or whatever) and all following is the comment
-		
-		#write post backup file
-		comment_file = open(comment_file_name, 'w')
-		comment_file.write(comment_id + '\n')
-		comment_file.write(user + '\n')
-		comment_file.write(timestamp + '\n')
-		comment_file.write(forum_category + '\n')
-		comment_file.write(forum_category_id + '\n')
-		comment_file.write(forum_name + '\n')
-		comment_file.write(forum_id + '\n')
-		comment_file.write(topic_title + '\n')
-		comment_file.write(topic_id + '\n')
-		comment_file.write('\n')
-		comment_file.write(post_text)
-		
-		
-		
-		
-		# write counter file
-		# we apparently can't trust configparser's write function because it uses dictionaries, so we simply replace line 4 or something
-		#f = open('test.ini', 'w'); f.write('blabla')
-		# maybe try this http://stackoverflow.com/questions/1877999/delete-final-line-in-file-via-python
-		
-		'''
-		readFile = open(config_file)
-
-		lines = readFile.readlines()
-
-		readFile.close()
-		w = open("file",'w')
-
-		w.writelines([item for item in lines[:-1]])
-
-		w.close()
-		'''
-	#else:
+	# Anyhoo, skip this iteration if False, because then we don't have read access.
+	if quote_request.ok is False:
 		#write failure to log file?
-		
-	# Wait before we go to the next post
-	wait()
+		print('Skipping '+comment_id+'. Authorization or other problem.')
+		continue
+	
+	quote_page = quote_request.text
+	#print(quote_page)
+	
+	metadata = re.search(r'<div id="forumnav"><p class="forumnav"><a href="/[\w]+/forums/">Forums</a>  » <a href="forum\.dml\?id=([0-9]+)">(.+?)</a> » <a href="topic\.dml\?id=[0-9]+">(.+?)</a>', quote_page)
+	
+	#print(metadata.group(0))
+	forum_id = metadata.group(1)
+	forum_name = metadata.group(2)
+	topic_title = metadata.group(3)
+	#print(forum_id)
+	#print(forum_name)
+	#print(topic_title)
+	
+	# re.DOTALL makes dot also match newlines
+	post = re.search(r'<textarea name="comment" id="postcontent" rows="20" cols="60">\[quote=([\w]+)](.+?)\[/quote] </textarea>', quote_page, re.DOTALL)
+	user = post.group(1)
+	post_text = post.group(2)
+	
+	# Decode HTML entities
+	# Thanks to http://stackoverflow.com/a/2087433
+	import html.parser
+	h = html.parser.HTMLParser()
+	post_text = h.unescape(post_text)
+	
+	#print(user)
+	#print(post_text)
+	
+	#################
+	#we're still missing the forum category; grab it using the topic id or forum id
+	#forum_id with perscreen=1 is most likely the smallest
+	category_request_url = 'http://my.opera.com/community/forums/forum.dml?id='+forum_id+'&perscreen=1'
+	category_request = requests.get(category_request_url, auth=credentials)
+	
+	metadata = re.search(r'<p class="forumnav"><a href="/[\w]+/forums/">Forums</a>   » <a href="/community/forums/tgr.dml\?id=([0-9]+)" dir="ltr">(.+?)</a>', category_request.text)
+	
+	forum_category_id = metadata.group(1)
+	forum_category = metadata.group(2)
+	
+	#print(forum_category_id)
+	#print(forum_category)
+	
+	
+	# write post file
+	# format something simple and logical, e.g.
+	'''
+	user
+	comment_id
+	timestamp
+	forum_category_id
+	forum_category
+	forum_id
+	forum_name
+	topic_id
+	topic_title
+
+	post_text
+	'''
+	# in this format line 1 is user name, line 2 is comment id, etc. line 10 (or whatever) and all following is the comment
+	
+	#write post backup file
+	comment_file = open(comment_file_name, 'w')
+	comment_file.write(comment_id + '\n')
+	comment_file.write(user + '\n')
+	comment_file.write(timestamp + '\n')
+	comment_file.write(forum_category + '\n')
+	comment_file.write(forum_category_id + '\n')
+	comment_file.write(forum_name + '\n')
+	comment_file.write(forum_id + '\n')
+	comment_file.write(topic_title + '\n')
+	comment_file.write(topic_id + '\n')
+	comment_file.write('\n')
+	comment_file.write(post_text)
+	
+	
+	
+	
+	# write counter file
+	# we apparently can't trust configparser's write function because it uses dictionaries, so we simply replace line 4 or something
+	#f = open('test.ini', 'w'); f.write('blabla')
+	# maybe try this http://stackoverflow.com/questions/1877999/delete-final-line-in-file-via-python
+	
+	'''
+	readFile = open(config_file)
+
+	lines = readFile.readlines()
+
+	readFile.close()
+	w = open("file",'w')
+
+	w.writelines([item for item in lines[:-1]])
+
+	w.close()
+	'''
+	
 	#counter+=1
